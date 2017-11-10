631649a74059bf9407a0e499f4f3244f489d9be2
JD Bothma <jbothma@gmail.com>
2017-11-10 08:21:14 +0200

    Add vote column and use fuzzyfuzzy

--- etl/normalise.py
+++ etl/normalise.py
@@ -1,37 +1,52 @@
-import distance
+from fuzzywuzzy import process
 import pandas as pd
 import os
 import re
 
-df = pd.read_json('etl-data/scraped.jsonl', lines=True)
+df_depts = pd.read_csv('metadata/departments.csv')
+df_files = pd.read_json('etl-data/scraped.jsonl', lines=True)
 
-years = set(df['year'].tolist())
-geographic_regions = set(df['geographic_region'].tolist())
+years = set(df_depts['financial_year'].tolist())
+geographic_regions = set(df_depts['geographic_region'].tolist())
 
 for year in years:
-    for juris in geographic_regions:
+    for region in geographic_regions:
         clean_sheets = {}
         clean_chapters = {}
-        names = df.loc[(df['geographic_region'] == juris) & (df['year'] == year)]['name'].tolist()
-        chapters = [name for name in names if 'Vote' in name]
-        for chapter in chapters:
+
+        names = df_files.loc[
+            (df_files['geographic_region'] == region) &
+            (df_files['year'] == year)
+        ]['name'].tolist()
+
+        chapter_names = df_depts.loc[
+            (df_depts['geographic_region'] == region) &
+            (df_depts['financial_year'] == year)
+        ]['chapter_title'].tolist()
+
+        for chapter in chapter_names:
             clean_chapters[re.sub('Vote \d+ : ', '', chapter)] = chapter
+
         sheets = [name for name in names if 'xls' in name and 'summary' not in name.lower()]
+
         for sheet in sheets:
             clean_sheets[re.sub('[A-Z]+ - ', '', os.path.splitext(sheet)[0])] = sheet
+
         for sheet in clean_sheets.keys():
             if 'cogta' in sheet.lower():
                 mod_sheet = 'Cooperative Governance and Traditional Affairs'
             elif 'finance' in sheet.lower():
+                # This shouldn't replace Treasury with Finance.
                 mod_sheet = 'Treasury'
             else:
                 mod_sheet = sheet
-            dist, chapter = sorted(distance.ilevenshtein(mod_sheet, clean_chapters.keys()))[0]
-            max_len = max(len(chapter), len(mod_sheet))
-            dist_pct = 100 * (max_len-dist) / len(chapter)
-            df.loc[(df['geographic_region'] == juris) &
-                   (df['year'] == year) &
-                   (df['name'] == clean_sheets[sheet]),
+            chapter = process.extractOne(mod_sheet, clean_chapters.keys())[0]
+            df_files.loc[(df_files['geographic_region'] == region) &
+                   (df_files['year'] == year) &
+                   (df_files['name'] == clean_sheets[sheet]),
                    'name'] = clean_chapters[chapter]
 
-df.to_csv('etl-data/scrape_normalised.csv')
+df_files.sort_values(['geographic_region', 'name'], inplace=True)
+df_files.sort_values('year', ascending=False, inplace=True, kind='mergesort')
+
+df_files.to_csv('etl-data/scrape_normalised_fuzzy.csv')
