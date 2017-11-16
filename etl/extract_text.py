62b6ca98ab9c4ecdb9e985756b994958806aba53
JD Bothma <jbothma@gmail.com>
2017-11-16 11:17:04 +0200

    Extract text and fix filenames

--- etl/extract_text.py
+++ etl/extract_text.py
@@ -9,24 +9,39 @@ with open('etl-data/scraped.jsonl') as jsonfile:
             original_filename = item['path']
             original_filename_noext, extension = os.path.splitext(original_filename)
 
-            filename_p1_3 = "%s-p1-3.pdf" % original_filename
+            filename_p1_3 = "%s-p1-3" % original_filename_noext
             if os.path.isfile(filename_p1_3):
                 print "File exists: %s" % filename_p1_3
             else:
                 split_cmd = ['pdftk', original_filename, 'cat', '1-3', 'output', filename_p1_3]
                 print split_cmd
                 exit_code = subprocess.call(split_cmd)
-                print exit_code
+                if exit_code:
+                    raise Exception("Exit code 1 for command %r" % split_cmd)
             print
 
-            ocr_filename = "%s-OCRd.pdf" % original_filename_noext
+            ocr_filename = "%s-OCRd.pdf" % filename_p1_3
             if os.path.isfile(ocr_filename):
                 print "File exists: %s" % ocr_filename
             else:
+                print "Doesn't exists: %s" % ocr_filename
+
                 ocr_cmd = "etl/ocr-pdf '%s'" % filename_p1_3
                 print ocr_cmd
                 exit_code = subprocess.call(ocr_cmd, shell=True)
-                print exit_code
+                if exit_code:
+                    raise Exception("Exit code 1 for command %r" % ocr_cmd)
+            print
+
+            txt_filename = "%s.txt" % original_filename_noext
+            if os.path.isfile(txt_filename):
+                print "File exists: %s" % txt_filename
+            else:
+                ocr_cmd = ["pdftotext", ocr_filename, txt_filename]
+                print ocr_cmd
+                exit_code = subprocess.call(ocr_cmd)
+                if exit_code:
+                    raise Exception("Exit code 1 for command %r" % txt_cmd)
             print
 
             print
