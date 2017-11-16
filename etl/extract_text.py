3026b67824126ee073fab0cb0431381cbe47afec
JD Bothma <jbothma@gmail.com>
2017-11-16 16:23:50 +0200

    WIP

--- etl/extract_text.py
+++ etl/extract_text.py
@@ -1,47 +1,122 @@
 import json
 import subprocess
 import os
+import regex as re
+import pprint
+
+
+def extract_pages(basename):
+    filename_p1_3 = "%s-p1-3.pdf" % basename
+    if os.path.isfile(filename_p1_3):
+        print "File exists: '%s'" % filename_p1_3
+    else:
+        command = ['pdftk', original_filename, 'cat', '1-3', 'output', filename_p1_3]
+        print command
+        exit_code = subprocess.call(command)
+        if exit_code:
+            raise Exception("Exit code %s for command %r" % (exit_code, command))
+    return filename_p1_3
+
+
+def ocr(basename, filename_p1_3):
+    filename_p1_3_noext = os.path.splitext(filename_p1_3)[0]
+    ocr_filename = "%s-OCRd.pdf" % filename_p1_3_noext
+    if os.path.isfile(ocr_filename):
+        print "File exists: '%s'" % ocr_filename
+    else:
+        print "Doesn't exists: '%s'" % ocr_filename
+        return
+        command = "etl/ocr-pdf '%s'" % filename_p1_3
+        print command
+        exit_code = subprocess.call(command, shell=True)
+        if exit_code:
+            raise Exception("Exit code %s for command %r" % (exit_code, command))
+    return ocr_filename
+
+
+def extract_text(basename, ocr_filename):
+    txt_filename = "%s.txt" % basename
+    if os.path.isfile(txt_filename):
+        print "File exists: '%s'" % txt_filename
+    else:
+        command = ["pdftotext", ocr_filename, txt_filename]
+        print command
+        exit_code = subprocess.call(command)
+        if exit_code:
+            raise Exception("Exit code %s for command %r" % (exit_code, command))
+    return txt_filename
+
+
+overview_matcher = re.compile(r'\n(?:1\.)?\s*Overview\n\s*(?P<overview_content>.+)(?:Review of the current financial year|$)', flags=re.DOTALL)
+
+headings = [
+    'Vision',
+    'Mission',
+    'Strategic Objectives',
+    'Overview of the main services that the department intends to deliver',
+    'Legislative mandate',
+    'External activities and other events relevant to budget decisions',
+    'Core functions and responsibilities',
+    'Acts, Rules and Regulations',
+]
+headings_regex = '(?:%s)\n' % '|'.join(headings)
+subsection_match_regex = r'^(\n[]*%s.+?)+$' % headings_regex
+subsection_split_regex = r'^(?:\n[]*(?P<heading>%s)(?P<content>.+?))+$' % headings_regex
+subsection_matcher = re.compile(subsection_match_regex, flags=re.DOTALL)
+subsection_splitter =  re.compile(subsection_split_regex, flags=re.DOTALL)
+
+print subsection_split_regex
+
+def text_to_markdown(basename, txt_filename):
+    dirname = "%s_markdown" % basename
+
+    with open(txt_filename) as txt_file:
+        text = txt_file.read()
+        match = overview_matcher.search(text)
+        if match:
+            print
+            print
+            overview_content = match.group('overview_content')
+            subsection_matches = subsection_matcher.match('\n' + overview_content)
+            if subsection_matches:
+                for subsection in subsection_matches.captures():
+                    split_subsection = subsection_splitter.match(subsection)
+                    print "### " + split_subsection.group('heading')
+                    print
+                    print "    " + split_subsection.group('content')
+                    print "----------------------"
+                print "======================================="
+            else:
+                print "---------------------"
+                print "No subsection matches"
+                print "---------------------"
+                print subsection_match_regex
+                print overview_content
+        else:
+            print
+            print
+            print "========== no overview section =========="
+
+
+
+    return dirname
+
 
 with open('etl-data/scraped.jsonl') as jsonfile:
     for line in jsonfile.readlines():
         item = json.loads(line)
         if item.get('path', '').endswith('.pdf'):
             original_filename = item['path']
-            original_filename_noext, extension = os.path.splitext(original_filename)
+            original_filename_noext = os.path.splitext(original_filename)[0]
 
-            filename_p1_3 = "%s-p1-3" % original_filename_noext
-            if os.path.isfile(filename_p1_3):
-                print "File exists: %s" % filename_p1_3
-            else:
-                split_cmd = ['pdftk', original_filename, 'cat', '1-3', 'output', filename_p1_3]
-                print split_cmd
-                exit_code = subprocess.call(split_cmd)
-                if exit_code:
-                    raise Exception("Exit code 1 for command %r" % split_cmd)
+            filename_p1_3 = extract_pages(original_filename_noext)
             print
 
-            ocr_filename = "%s-OCRd.pdf" % filename_p1_3
-            if os.path.isfile(ocr_filename):
-                print "File exists: %s" % ocr_filename
-            else:
-                print "Doesn't exists: %s" % ocr_filename
-
-                ocr_cmd = "etl/ocr-pdf '%s'" % filename_p1_3
-                print ocr_cmd
-                exit_code = subprocess.call(ocr_cmd, shell=True)
-                if exit_code:
-                    raise Exception("Exit code 1 for command %r" % ocr_cmd)
+            ocr_filename = ocr(original_filename_noext, filename_p1_3)
             print
 
-            txt_filename = "%s.txt" % original_filename_noext
-            if os.path.isfile(txt_filename):
-                print "File exists: %s" % txt_filename
-            else:
-                ocr_cmd = ["pdftotext", ocr_filename, txt_filename]
-                print ocr_cmd
-                exit_code = subprocess.call(ocr_cmd)
-                if exit_code:
-                    raise Exception("Exit code 1 for command %r" % txt_cmd)
+            txt_filename = extract_text(original_filename_noext, ocr_filename)
             print
 
+            markdown_folder = text_to_markdown(original_filename, txt_filename)
             print
