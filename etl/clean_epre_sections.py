82bccda6a387515f24d8223f3090a71c407ece47
JD Bothma <jbothma@gmail.com>
2017-11-29 12:44:07 +0200

    handle known empties/incompletes

--- etl/clean_epre_sections.py
+++ etl/clean_epre_sections.py
@@ -8,6 +8,13 @@ basedir = '../data/provincial/temps'
 
 count = 0
 
+incompletes = set()
+with open('../data/provincial/temps/missing.csv') as missings_file:
+    reader = csv.DictReader(missings_file)
+    for row in reader:
+        incompletes.add(os.path.join(basedir, row['text_file']))
+
+
 for year in os.listdir(basedir):
     if re.match(r'\d{4}', year):
         year_path = os.path.join(basedir, year)
@@ -18,19 +25,21 @@ for year in os.listdir(basedir):
                     if filename.endswith('.txt'):
                         file_path = os.path.join(geographic_region_path, filename)
                         with open(file_path) as text_file:
-                            file_text = text_file.read()
-                            front_matter, content = file_text.split("---")
-                            front_matter = front_matter.strip()
-                            metadata = {}
                             print
                             print "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
                             print file_path
+
+                            file_text = text_file.read()
+                            front_matter, content = file_text.split("---")
+                            front_matter = front_matter.strip()
+                            print front_matter
+                            metadata = {}
                             for line in front_matter.split("\n"):
                                 key, value = line.split(":", 1)
                                 metadata[key] = value.strip()
                             metadata['department_name'] = metadata['name'].split(':')[1]
 
-                            print metadata
+                            #print metadata
 
                             regex = r"^\s*(?:# *(?P<heading>[\w ]+)[\r\n]+(?P<content>[^#]+))+\s*$"
                             match = re.match(regex, content, flags=re.MULTILINE)
@@ -41,5 +50,9 @@ for year in os.listdir(basedir):
                                 print count
                             else:
                                 print "%s" % content
-                                if not content.strip() == "":
-                                    raise Exception()
+                                if file_path in incompletes:
+                                    print "#### known incomplete"
+                                else:
+                                    print os.path.join(year, geographic_region, filename)
+                                    print
+                                    raise Exception("%r" % file_path)
