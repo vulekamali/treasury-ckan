8b3c3bb57e4fce58a05f1d4f5bc1ed8de393151b
JD Bothma <jbothma@gmail.com>
2017-11-29 17:49:55 +0200

    Write EPRE section files and list

--- etl/clean_epre_sections.py
+++ etl/clean_epre_sections.py
@@ -7,7 +7,7 @@ import yaml
 basedir = '../data/provincial/temps'
 
 
-def parse_file(text_file):
+def parse_file(text_file, epre_sections_writer):
     print
     print "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
     print file_path
@@ -27,11 +27,26 @@ def parse_file(text_file):
     regex = r"^\s*(?P<section># *(?P<heading>[\w ]+)[\r\n]+(?P<content>[^#]+))+\s*$"
     match = re.match(regex, content, flags=re.MULTILINE)
     if match:
+        dirlist = ['etl-data', metadata['year'], metadata['geographic_region'], metadata['name']]
+        dirname = os.path.join(*dirlist)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
         captures = match.capturesdict()
         for idx, section in enumerate(captures['section']):
-            print captures['heading'][idx]
-            print "=========\n"
-            print captures['content'][idx]
+            heading = captures['heading'][idx]
+            content = captures['content'][idx]
+            section_filename = os.path.join(dirname, heading + ".md")
+            with open(section_filename, 'wb') as section_file:
+                section_file.write(content)
+            epre_sections_writer.writerow({
+                'department_name': metadata['department_name'],
+                'financial_year': metadata['year'],
+                'sphere': 'provincial',
+                'heading': heading,
+                'path': section_filename,
+                'geographic_region': metadata['geographic_region'],
+            })
         return True
     else:
         print "%s" % content
@@ -61,18 +76,29 @@ with open('../data/provincial/temps/autoblanks.csv') as autoblanks_file:
     for row in reader:
         incompletes.add(os.path.join(basedir, row['text_file']))
 
-with open('../data/provincial/temps/autoblanks.csv', 'wb') as autoblanks:
-    autoblanks.write("text_file\n")
-    for year in os.listdir(basedir):
-        if re.match(r'\d{4}', year):
-            year_path = os.path.join(basedir, year)
-            for geographic_region in os.listdir(year_path):
-                if re.match(r'[\w ]+', geographic_region):
-                    geographic_region_path = os.path.join(year_path, geographic_region)
-                    for filename in os.listdir(geographic_region_path):
-                        if filename.endswith('.txt'):
-                            file_path = os.path.join(geographic_region_path, filename)
-                            with open(file_path) as text_file:
-                                if parse_file(text_file):
-                                    count += 1
-                                    print count
+with open('etl-data/epre_sections.csv', 'wb') as epre_sections:
+    headings = [
+        'department_name',
+        'financial_year',
+        'sphere',
+        'heading',
+        'path',
+        'geographic_region',
+    ]
+    epre_sections_writer = csv.DictWriter(epre_sections, fieldnames=headings)
+    epre_sections_writer.writeheader()
+    with open('../data/provincial/temps/autoblanks.csv', 'wb') as autoblanks:
+        autoblanks.write("text_file\n")
+        for year in os.listdir(basedir):
+            if re.match(r'\d{4}', year):
+                year_path = os.path.join(basedir, year)
+                for geographic_region in os.listdir(year_path):
+                    if re.match(r'[\w ]+', geographic_region):
+                        geographic_region_path = os.path.join(year_path, geographic_region)
+                        for filename in os.listdir(geographic_region_path):
+                            if filename.endswith('.txt'):
+                                file_path = os.path.join(geographic_region_path, filename)
+                                with open(file_path) as text_file:
+                                    if parse_file(text_file, epre_sections_writer):
+                                        count += 1
+                                        print count
