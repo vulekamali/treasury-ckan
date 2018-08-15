c9dc94e16f06db0e01392c8263f0628cc176565d
JD Bothma <jbothma@gmail.com>
2018-08-15 17:07:34 +0200

    Replace remote files with uploaded versions

--- etl/upload_urls.py
+++ etl/upload_urls.py
@@ -1,7 +1,30 @@
 from ckanapi import RemoteCKAN
 from collections import defaultdict
+from tempfile import mkdtemp
 
 import argparse
+import requests
+import os
+import urlparse
+import urllib
+
+upload_formats = [
+    'CSV',
+    'PDF',
+    'XLS',
+    'XLSB',
+    'XLSM',
+    'XLSX',
+]
+
+
+def download_file(url):
+    tempdir = mkdtemp(prefix="budgetportal")
+    print "Downloading %s" % url
+    basename = os.path.basename(urlparse.urlparse(url).path)
+    filename = os.path.join(tempdir, basename)
+    return tempdir, urllib.urlretrieve(url, filename)[0]
+
 
 parser = argparse.ArgumentParser(description='Bring CKAN up to date with a local representation of a set of datasets')
 
@@ -9,6 +32,7 @@ parser.add_argument('--apikey', help='authentication key')
 parser.add_argument('--instance', help='CKAN instance root URL')
 parser.add_argument('--count-formats', default=False, action='store_true', help='Count the number of non-uploaded files in each format')
 parser.add_argument('--list-urls', default=False, action='store_true', help='List the non-uploaded URLs')
+parser.add_argument('--replace-urls', default=False, action='store_true', help='Replace non-uploaded resources with an uploaded version.')
 
 args = parser.parse_args()
 
@@ -32,3 +56,12 @@ if args.count_formats:
 if args.list_urls:
     for resource in non_uploads:
         print resource['format'], resource['url']
+
+if args.replace_urls:
+    for resource in non_uploads:
+        if resource['format'] in upload_formats:
+            dirname, filename = download_file(resource['url'])
+            print "Uploading %s" % filename
+            ckan.action.resource_patch(id=resource['id'], upload=open(filename, 'rb'))
+            os.remove(filename)
+            os.rmdir(dirname)
